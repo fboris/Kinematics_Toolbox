b2a31c0e833fef95a71585944d767178462b3938
autonomoid <autonomoid@users.noreply.github.com>
2015-07-06 11:44:04 +0100

    Added 6DOF IDK example

---
+++ examples/idk_6R.m
@@ -0,0 +1,65 @@
+% Inverse Differential Kinematics (IDK) Example Using RRR Planar Arm 
+
+clear;
+close all;
+
+global DebugLevel;
+DebugLevel = 1;
+
+% Specify the link twist co-ordinates.
+% For revolute joint we use the following form:
+% [-w1 x q1, -w2 x q2, -w3 x q3, w1, w2, w3]
+
+% 6-link non-planar RRR arm
+lengthA = 1;
+lengthB = 1;
+lengthC = 1;
+lengthD = 1;
+lengthE = 1;
+lengthF = 1;
+lengthG = 1;
+
+joint1 = [0; 0; 0; 0; 0; 1];
+joint2 = [-(lengthA + lengthB); 0; 0; 0; 1; 0];
+joint3 = [0; (lengthA + lengthB); 0; 1; 0; 0];
+joint4 = [-(lengthA + lengthB); 0; (lengthC + lengthD); 0; 1; 0];
+joint5 = [0; -(lengthC + lengthD); 0; 0; 0; 1];
+joint6 = [-(lengthA + lengthB + lengthE + lengthF); 0; (lengthC + lengthD); 0; 1; 0];
+
+% Specify the end-effector reference/home pose
+% as a homogeneous matrix.
+% E.g. the end-effector is at [x,y,z] = [0, 2, 0]
+% with no rotation.
+M = eye(4);
+M(1, 4) = (lengthC + lengthD);
+M(3, 4) = (lengthA + lengthB + lengthE + lengthF + lengthG);
+
+% Create the robot
+RRR = robot({joint1, joint2, joint3, joint4, joint5, joint6}, M);
+
+% Compute the IDK for a constant end-effectory velocity of v_x = -0.1m/s for 10s.
+% I.e. dx = 25cm. We expect the end-effector to move from (2, 0, 0) to (2.25, 0, 0).
+n_trajectory_entries = 200;
+dt = 0.05;
+desired_end_effector_body_velocity = [-0.1; 0; 0; 0; 0; 0];
+joint_angles = zeros(6, n_trajectory_entries);
+
+% Fill the trajectory.
+for i=1:n_trajectory_entries
+  % Evaluate the body jacobian at the current joint angles.
+  body_jacobian = bjacob(RRR, joint_angles(:,i));
+  inv_body_jacobian = pinv(body_jacobian);
+
+  % Compute the joint velocities that will achieve the desired end-effector body velocity.
+  joint_velocities = inv_body_jacobian * desired_end_effector_body_velocity;
+
+  % Compute the next trajectory point using simple integration.
+  joint_angles(:, i+1) = joint_angles(:, i) + (dt * joint_velocities);
+end
+
+% For display purposes convert the joint-space trajectory into an end-effector trajectory
+end_effector_trajectory = fkine(RRR, joint_angles);
+
+% Plot the end-effector trajectory
+named_figure('Cartesian Trajectory of the End-Effector');
+drawframetraj(end_effector_trajectory);
--- examples/non_planar_6R.m
+++ examples/non_planar_6R.m
@@ -10,31 +10,41 @@ DebugLevel = 1;
 % For revolute joint we use the following form:
 % [-w1 x q1, -w2 x q2, -w3 x q3, w1, w2, w3]
 
-% 3-link non-planar RRR arm
-lengthA = 2;
-lengthB = 3;
+% 6-link non-planar RRR arm
+lengthA = 1;
+lengthB = 1;
 lengthC = 1;
+lengthD = 1;
+lengthE = 1;
+lengthF = 1;
+lengthG = 1;
 
-joint1 = [0; 0; 0; 0; 1; 0];
-joint2 = [0; 0; 0; 0; 0; 1];
-joint3 = [-(lengthA + lengthB); 0; 0; 0; 0; 1];
+joint1 = [0; 0; 0; 0; 0; 1];
+joint2 = [-(lengthA + lengthB); 0; 0; 0; 1; 0];
+joint3 = [0; 0; 0; 0; 0; 1];
+joint4 = [-(lengthA + lengthB + lengthC + lengthD); 0; 0; 0; 1; 0];
+joint5 = [0; 0; 0; 0; 0; 1];
+joint6 = [-(lengthA + lengthB + lengthC + lengthD + lengthE + lengthF); 0; 0; 0; 1; 0];
 
 % Specify the end-effector reference/home pose
 % as a homogeneous matrix.
 % E.g. the end-effector is at [x,y,z] = [0, 2, 0]
 % with no rotation.
 M = eye(4);
-M(3,4) = lengthA + lengthB + lengthC;
-%M(1, 4) = 4;
+M(3,4) = (lengthA + lengthB + lengthC + lengthD + lengthE + lengthF + lengthG);
 
 % Create the robot
-RRR = robot({joint1, joint2, joint3}, M);
+RRR = robot({joint1, joint2, joint3, joint4, joint5, joint6}, M);
 
 % Create a trajectory in joint space, only moving joint 2.
 q1 = [-pi/2:pi/16:pi/2];
 q2 = [-pi/2:pi/16:pi/2];
+%q1 = zeros(size(q2));
 q3 = zeros(size(q2));
-joint_space_trajectory = [q1; q2; q3]
+q4 = zeros(size(q2));
+q5 = zeros(size(q2));
+q6 = zeros(size(q2));
+joint_space_trajectory = [q1; q2; q3; q4; q5; q6]
 
 % Convert the joint-space trajectory into the end-effector
 % trajectory using the forward-kinematics.
@@ -45,15 +55,15 @@ named_figure('Cartesian Trajectory of the End-Effector');
 drawframetraj(end_effector_trajectory);
 
 % Compute the body Jacobian at reference pose (vertical)
-body_jacobian = bjacob(RRR, [0; 0; 0])
+body_jacobian = bjacob(RRR, [0; 0; 0; 0; 0; 0])
 
 % Calculate the instantaneous end-effector body velocity for a
 % given set of joint velocities
-body_velocity = body_jacobian * [0; 0.1; 0]
+body_velocity = body_jacobian * [0; 0.1; 0; 0; 0; 0]
 
 % Evalute the spatial Jacobian also at the reference pose. 
-spatial_jacobian = sjacob(RRR, [0; 0; 0])
+spatial_jacobian = sjacob(RRR, [0; 0; 0; 0; 0; 0])
 
 % Calculate the instantaneous end-effector spatial velocity for a
 % given set of joint velocities
-spatial_velocity = spatial_jacobian * [0; 0.1; 0]
+spatial_velocity = spatial_jacobian * [0; 0.1; 0; 0; 0; 0]
